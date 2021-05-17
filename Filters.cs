34399daf34aab11239913aeb61d57e5417bd97ea
NotSoSaneExile <sciemastyle@wp.pl>
2021-05-17 13:16:42 +0200

    Add project files.

---
+++ Filters.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Filters
+{
+    public class CustomFilterAttributes : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var ip = context.HttpContext.Connection.RemoteIpAddress;
+            ip = System.Net.Dns.GetHostEntry(ip).AddressList.First(x => x.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+            ip.ToString();
+
+            var result = context.Result;
+
+            if(result is PageResult)
+            {
+                var page = ((PageResult)result);
+                page.ViewData["filterMessage"] = $"Yours IP: {ip}";
+            }
+            await next.Invoke();
+        }
+    }
+}
